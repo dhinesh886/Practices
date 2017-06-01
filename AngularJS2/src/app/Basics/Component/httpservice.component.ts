f09dfc4ca24f4044836e11f2bec7a9641463d3ae
Dhineshkumar M <dhinesh886@gmail.com>
2017-06-01 11:14:38 +0530

    http service
    http service

--- AngularJS2/src/app/Basics/Component/httpservice.component.ts
+++ AngularJS2/src/app/Basics/Component/httpservice.component.ts
@@ -1,24 +1,37 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostService } from '../Service/post.service';
 @Component({
-    selector : 'http-service',
-    templateUrl : '../../Templates/httpservice.template.html',
-    providers : [PostService]
+    selector: 'http-service',
+    templateUrl: '../../Templates/httpservice.template.html',
+    providers: [PostService]
 })
 
 export class HttpServiceComponent implements OnInit {
-    posts : any;
-    constructor(private _service : PostService)
-    {
+    post: any;
+    posts: any;
+    isLoading: boolean = true;
+    constructor(private _service: PostService) {
 
     }
-ngOnInit()
-{
-    this._service.getPosts()
-    .subscribe(res => this.getPostsCallback(res));
+    ngOnInit() {
+        this._service.getPosts()
+            .subscribe(res => this.getPostsCallback(res));
+    }
+    getPostsCallback(res: any) {
+        this.posts = res;
+        this.isLoading = false;
+    }
+    OnClick(postid: Number) {
+        console.log("1");
+        this.isLoading = true;
+        this.post = null;
+        this._service.getPost(postid)
+            .subscribe(res => {
+                console.log("2");
+                this.post = res;
+                this.isLoading = false;
+            });
+        console.log("3");
+    }
 }
-getPostsCallback(res: any)
-{
-    this.posts = res;
-}
-}
+
--- AngularJS2/src/app/Basics/Service/post.service.ts
+++ AngularJS2/src/app/Basics/Service/post.service.ts
@@ -1,28 +1,24 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
+import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Post } from '../Models/Post.model';
 
 @Injectable()
 export class PostService {
 
-constructor (private _http: Http)
-{
+    constructor(private _http: Http) {
 
-}
- getPosts() 
- {
-     return this._http.get("https://jsonplaceholder.typicode.com/posts")
-     .map(response => response.json());
- }
-    getPost(id:Number)
- {
-     return this._http.get("https://jsonplaceholder.typicode.com/posts/"+id.toString())
-     .map(response => response.json());
- }
- createPost(post : Post)
- {
-     return this._http.get("https://jsonplaceholder.typicode.com/posts", JSON.stringify(post))
-     .map(response => response.json());
- }
+    }
+    getPosts() {
+        return this._http.get("https://jsonplaceholder.typicode.com/posts")
+            .map(response => response.json());
+    }
+    getPost(id: Number) {
+        return this._http.get("https://jsonplaceholder.typicode.com/posts/" + id.toString())
+            .map(response => response.json());
+    }
+    createPost(post: Post) {
+        return this._http.get("https://jsonplaceholder.typicode.com/posts", JSON.stringify(post))
+            .map(response => response.json());
+    }
 }
